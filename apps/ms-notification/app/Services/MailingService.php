56f83ecff04b93fa0418cc353519385cb6b7666a
Harvey.Huang <huang63261@gmail.com>
2024-04-10 16:59:20 +0800

    chore: `MailingService` 類別屬性修正

--- apps/ms-notification/app/Services/MailingService.php
+++ apps/ms-notification/app/Services/MailingService.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Mail;
 
 class MailingService
 {
-    public static $STATUS_MAP = [
+    private const STATUS_MAP = [
         '0' => 'Order pending',
         '1' => 'Order processing',
         '2' => 'Order completed',
@@ -17,7 +17,7 @@ class MailingService
     public static function sendMail(array $order)
     {
         $order = (object) $order;
-        $order->status = self::$STATUS_MAP[$order->status];
+        $order->status = self::STATUS_MAP[$order->status];
 
         Mail::to("customer@example.com")->send(new OrderCompleted($order));
     }
